928cfa4b3e036a479133f8c79cd7666b3972b8fd
Luis-Mocha <luismocha97@gmail.com>
2023-06-08 17:18:00 +0200

    nav iniziale con link routing

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,10 @@
-header
+<nav>
+    <ul>
+        <li>
+            <a href="{{ route('home') }}">Home</a>
+        </li>
+        <li>
+            <a href="{{ route('comics') }}">Comics</a>
+        </li>
+    </ul>
+</nav>
