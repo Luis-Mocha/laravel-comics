1382616873713ce796b46b4fd90aef1335cbbe0a
Luis-Mocha <luismocha97@gmail.com>
2023-06-08 19:26:00 +0200

    stampo ogni card (thumb non funziona)

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -6,6 +6,19 @@ DC-Comics | Comics
 
 @section( 'content-main' )
 
-    <h1>Pagina Comics</h1>
+    <div id="comics-main" class="container">
+        @foreach( $comics as $elem )
+            
+        <div class="comic-card">
+            <!-- <img src=" {{ $elem['thumb'] }} " alt="Thumb">  -->
+            <!-- il link pe la thumb non funziona-->
+
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+
+            <div class="cardTitle">{{$elem['title']}}</div>
+        </div>
+
+        @endforeach
+    </div>
 
 @endsection
