3aa07aad43517dba2b0a7645bd47deba56134f5b
Luis-Mocha <luismocha97@gmail.com>
2023-06-09 00:46:14 +0200

    footer struttura e stile

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,29 @@
-<div>footer section - work in progress</div>
+<footer>
+    <div class="socialComp">
+
+        <div class="container">
+            <button class="signButton">
+                sign-up now
+            </button>
+
+            <div class="social-list">
+                <span>follow us</span>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Icona facebook">
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Icona twitter">                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="Icona youtube">
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Icona pinterest">
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Icona periscope">
+                </a>
+            </div>
+        </div>
+
+    </div>
+</footer>
