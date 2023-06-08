9f1df5f82a48fe5a868e9a7569b339cc6f9510d6
Luis-Mocha <luismocha97@gmail.com>
2023-06-08 23:55:55 +0200

    ripristino pagina iniziale

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
+Route::get('/', function () {
 
     return view('pages.home');
 
 })->name('home');
 
 
-Route::get('/', function () {
+Route::get('/comics', function () {
 
     $comics = config( 'dbComics' );
 
